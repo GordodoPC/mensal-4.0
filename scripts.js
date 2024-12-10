fa30738179891ddea7574d210097dceb2747c39a
GordodoPC <eduardomciusz@hotmail.com>
2024-12-10 19:27:03 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Example of adding a simple event listener for scroll effect or functionality.
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
